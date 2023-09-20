353ef41059c8a76875d17341dc8e1be68cf42f43
Ekaterina <yatakshilina@gmail.com>
2023-09-20 18:34:08 -0400

    change size of search button

--- weatherapp/src/app/page.js
+++ weatherapp/src/app/page.js
@@ -4,6 +4,8 @@ import axios from "axios";
 
 import { useState } from "react";
 import { BsSearch } from "react-icons/bs";
+import Weather from "../../components/Weather";
+import Spinner from "../../components/Spinner";
 
 export default function Home() {
   const [city, setCity] = useState("");
@@ -17,7 +19,7 @@ export default function Home() {
       .get(url)
       .then((response) => {
         setWeather(response.data);
-        console.log(response.data);
+        //console.log(response.data);
       })
       .catch((error) => {
         console.log(error, "something went wrong");
@@ -26,39 +28,50 @@ export default function Home() {
     setLoading(false);
   };
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=toronto&units=metric&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`;
   console.log(city);
 
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        {/* OverLay */}
-        <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/40 z-[1]" />
+  if (loading) {
+    return <Spinner />;
+  } else {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div>
+          {/* OverLay */}
+          <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/10 z-[1]" />
 
-        <Image
-          src="https://images.unsplash.com/photo-1580193769210-b8d1c049a7d9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2374&q=80"
-          layout="fill"
-          alt="weather image"
-          className="object cover"
-        />
+          <Image
+            src="https://images.unsplash.com/photo-1580193769210-b8d1c049a7d9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2374&q=80"
+            layout="fill"
+            alt="weather image"
+            className="object cover"
+          />
 
-        {/* search */}
-        <div className="relative flex justify-between items-center max-w-[500px] w-full m-auto pt-4 text-white z-10">
-          <form className="flex justify-between items-center w-full m-auto p-3 bg-transparent border border-gray-300 text-white rounded-2xl">
-            <div>
-              <input
-              onChange={(e)=>setCity(e.target.value)}
-                className="bg-transparent border-none text-white focus:outline-none text-2xl"
-                type="text"
-                placeholder="Search city"
-              ></input>
-            </div>
-            <button onClick={fetchWeather}>
-              <BsSearch size={20} />
-            </button>
-          </form>
+          {/* search */}
+          <div className="relative flex justify-between items-center max-w-[500px] w-full m-auto pt-4 text-white z-10 ">
+
+            <form
+              onSubmit={fetchWeather}
+              className="flex justify-between items-center w-full m-auto p-3 bg-transparent border border-gray-300 text-white rounded-2xl"
+            >
+              <div>
+                <input
+                  onChange={(e) => setCity(e.target.value)}
+                  className="bg-transparent border-none text-white focus:outline-none text-3xl"
+                  type="text"
+                  placeholder="Search city"
+                ></input>
+              </div>
+              <button onClick={fetchWeather}>
+                <BsSearch size={25} />
+              </button>
+            </form>
+          </div>
+
+          {/* weather to display */}
+          {weather.main && <Weather data={weather} />}
         </div>
-      </div>
-    </main>
-  );
+      </main>
+    );
+  }
 }
